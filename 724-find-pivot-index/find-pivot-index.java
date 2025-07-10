8ebc17bfcec5a3e42e11c32716aeb20a7983cddb
Amit Kumar <69569478+amit4996@users.noreply.github.com>
2025-07-10 13:10:44 +0530

    Time: 1 ms (92.85%) | Memory: 45.3 MB (77.99%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -4,9 +4,7 @@ class Solution {
         for(int i=1;i<n;i++){
             nums[i]+=nums[i-1];
         }
-        for(int i=0;i<n;i++){
-            System.out.println(nums[i]);
-        }
+
 
         for(int i=0;i<n;i++){
             if(i==0 && nums[n-1]-nums[0]==0) return i;
