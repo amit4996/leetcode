ad499f2522607adb5480d00731546a0f4f00cbbf
Amit Kumar <69569478+amit4996@users.noreply.github.com>
2025-12-13 23:59:22 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (51.53%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -1,10 +1,22 @@
 class Solution {
-    public int hammingWeight(int n) {
+    // public int hammingWeight(int n) {
+    //     int count=0;
+    //     while(n>0){
+    //         if((n&1)==1) count++;//== has higher precedene in java so need backets
+    //         n=n>>1;
+    //     }
+    //     return count;
+    // }
+    // TC O(n)
+
+    //Brian Kernighan’s Algorithm for counting set bits
+        public int hammingWeight(int n) {
         int count=0;
-        while(n>0){
-            if((n&1)==1) count++;//== has higher precedene in java so need backets
-            n=n>>1;
+        while(n!=0){
+            n=n&(n-1);
+            count++;
         }
         return count;
     }
+    //TC O(set bits)
 }
