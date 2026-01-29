5b62fee302080c03c5923e2e5f90d2e53fd70ae9
Amit Kumar <69569478+amit4996@users.noreply.github.com>
2026-01-29 19:23:32 +0530

    Time: 1 ms (81.61%) | Memory: 43.9 MB (34.18%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -14,30 +14,60 @@
  * }
  */
 class Solution {
+    // public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    //     //BFS using boolean and reverse logic 
+    //     List<List<Integer>> result = new ArrayList<>();
+    //     if(root == null ) return result;
+    //     Queue<TreeNode> q = new LinkedList<>();
+    //     q.offer(root);
+    //     boolean isLeftToRight = true;
+    //     while(!q.isEmpty()){
+    //         int size =  q.size();
+    //         List<Integer> level = new ArrayList<>();
+    //         for(int i=0;i<size;i++){
+    //             TreeNode curr = q.poll();
+    //             level.add(curr.val);
+    //             if(curr.left !=null) q.offer(curr.left);
+    //             if(curr.right != null) q.offer(curr.right);
+    //         }
+    //         if(isLeftToRight != true){
+    //             Collections.reverse(level);
+    //         }
+    //         result.add(level);
+    //         isLeftToRight=!isLeftToRight;
+    //     }
+
+    //     return result;
+
+    // }
+
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        //BFS
+        //BFS using deque , decide order of insertion
         List<List<Integer>> result = new ArrayList<>();
         if(root == null ) return result;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.offerFirst(root);
         boolean isLeftToRight = true;
         while(!q.isEmpty()){
             int size =  q.size();
             List<Integer> level = new ArrayList<>();
             for(int i=0;i<size;i++){
-                TreeNode curr = q.poll();
-                level.add(curr.val);
-                if(curr.left !=null) q.offer(curr.left);
-                if(curr.right != null) q.offer(curr.right);
-            }
-            if(isLeftToRight != true){
-                Collections.reverse(level);
+                if(isLeftToRight == true){
+                    TreeNode curr = q.pollFirst();
+                    level.add(curr.val);
+                    if(curr.left !=null) q.offerLast(curr.left);
+                    if(curr.right != null) q.offerLast(curr.right);
+                }else{
+                    TreeNode curr = q.pollLast();
+                    level.add(curr.val);
+                    if(curr.right != null) q.offerFirst(curr.right);
+                    if(curr.left !=null) q.offerFirst(curr.left);
+                }
+                
             }
             result.add(level);
             isLeftToRight=!isLeftToRight;
         }
-
         return result;
-
     }
 }
