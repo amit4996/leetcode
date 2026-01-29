b73a2bb34a20edcc910c77ec4d05d594519ce090
Amit Kumar <69569478+amit4996@users.noreply.github.com>
2026-01-29 16:54:28 +0530

    Time: 1 ms (95.30%) | Memory: 46.9 MB (32.36%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -14,25 +14,45 @@
  * }
  */
 class Solution {
+    // public List<List<Integer>> levelOrder(TreeNode root) {
+    //     //BFS
+    //     List<List<Integer>> result = new ArrayList<>();
+    //             if(root==null) return result;
+
+    //     Queue<TreeNode> queue = new LinkedList<>();
+
+    //     queue.offer(root);
+
+    //     while(!queue.isEmpty()){
+    //         int size = queue.size();
+    //         List<Integer> level = new ArrayList<>();
+    //         for(int i=0;i<size;i++){
+    //             TreeNode curr = queue.poll();
+    //             level.add(curr.val);
+    //             if(curr.left!=null) queue.offer(curr.left);
+    //             if(curr.right!=null) queue.offer(curr.right);
+    //         }
+    //         result.add(level);
+    //     }
+    //     return result;
+    // }
+
     public List<List<Integer>> levelOrder(TreeNode root) {
+        //DFS
         List<List<Integer>> result = new ArrayList<>();
                 if(root==null) return result;
-
-        Queue<TreeNode> queue = new LinkedList<>();
-
-        queue.offer(root);
-
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            List<Integer> level = new ArrayList<>();
-            for(int i=0;i<size;i++){
-                TreeNode curr = queue.poll();
-                level.add(curr.val);
-                if(curr.left!=null) queue.offer(curr.left);
-                if(curr.right!=null) queue.offer(curr.right);
-            }
-            result.add(level);
-        }
+        levelOrderHelper(root,0,result);
         return result;
+        
+    }
+
+    public void levelOrderHelper(TreeNode node,int depth,List<List<Integer>> result){
+        if(node == null) return;
+        if(depth == result.size()){
+            result.add(new ArrayList<>());
+        }
+        result.get(depth).add(node.val);
+        if(node.left!=null) levelOrderHelper(node.left,depth+1,result);
+        if(node.right!=null) levelOrderHelper(node.right,depth+1,result);
     }
 }
