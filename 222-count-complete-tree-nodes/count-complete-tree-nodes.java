d88f4a972d39feb99db1bce1af7c4dd6ab9f7e84
Amit Kumar <69569478+amit4996@users.noreply.github.com>
2026-01-31 12:06:38 +0530

    Time: 0 ms (100.00%) | Memory: 49.4 MB (79.87%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -14,12 +14,49 @@
  * }
  */
 class Solution {
-    public int countNodes(TreeNode root) {
-        return helper(root);
+    // traversal way o(n)
+    // public int countNodes(TreeNode root) {
+    //     return helper(root);
+    // }
+
+    // public int helper(TreeNode root){
+    //     if(root==null) return 0;
+    //     return 1+helper(root.left)+helper(root.right);
+    // }
+
+    public int leftHeight(TreeNode root){
+        int count=0;
+        if(root==null) return count;
+        while(root.left!=null){
+            root=root.left;
+            count++;
+        }
+        return count;
     }
 
-    public int helper(TreeNode root){
+    public int rightHeight(TreeNode root){
+        int count=0;
+        if(root==null) return count;
+        while(root.right!=null){
+            root=root.right;
+            count++;
+        }
+        return count;
+    }
+    
+    public int helper(TreeNode root) {
         if(root==null) return 0;
+        int leftH=leftHeight(root);
+        int rightH=rightHeight(root);
+        if(leftH==rightH){
+            // return 2^(leftHeight(root)+1)-1;
+            return (int)Math.pow(2,leftH+1)-1;
+        }
         return 1+helper(root.left)+helper(root.right);
     }
+
+        public int countNodes(TreeNode root) {
+        return helper(root);
+    }
+    
 }
