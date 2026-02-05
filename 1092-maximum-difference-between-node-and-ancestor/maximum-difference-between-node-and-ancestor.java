f2a326df961af8fe255d1ab18219d9fd5db0aa93
Amit Kumar <69569478+amit4996@users.noreply.github.com>
2026-02-05 20:36:01 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (19.77%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
@@ -16,28 +16,32 @@
 class Solution {
     int globalMax=0;
     public int maxAncestorDiff(TreeNode root) {
-        List<Integer> path = new ArrayList<>();
+        // List<Integer> path = new ArrayList<>();
         int currentMaxSoFar=Integer.MIN_VALUE;
         int currentMinSoFar=Integer.MAX_VALUE;
-        helper(root,path,currentMaxSoFar,currentMinSoFar);
+        helper(root,currentMaxSoFar,currentMinSoFar);
         return globalMax;
     }
 
-    public void helper(TreeNode node,List<Integer> path,int currentMaxSoFar,int currentMinSoFar){
+    public void helper(TreeNode node,int currentMaxSoFar,int currentMinSoFar){
         if(node==null) return;
 
-        path.add(node.val);
+        // path.add(node.val);
 
-        for(int i=0;i<path.size();i++){
-            currentMaxSoFar=Math.max(currentMaxSoFar,path.get(i));
-            currentMinSoFar=Math.min(currentMinSoFar,path.get(i));
-        }
+        // for(int i=0;i<path.size();i++){
+        //     currentMaxSoFar=Math.max(currentMaxSoFar,path.get(i));
+        //     currentMinSoFar=Math.min(currentMinSoFar,path.get(i));
+        // }
 
+        currentMaxSoFar=Math.max(currentMaxSoFar,node.val);
+        currentMinSoFar=Math.min(currentMinSoFar,node.val);
+        
         globalMax=Math.max(globalMax,currentMaxSoFar-currentMinSoFar);
-        helper(node.left,path,currentMaxSoFar,currentMinSoFar);
-        helper(node.right,path,currentMaxSoFar,currentMinSoFar);
 
-        path.remove(path.size()-1);
+        helper(node.left,currentMaxSoFar,currentMinSoFar);
+        helper(node.right,currentMaxSoFar,currentMinSoFar);
+
+        // path.remove(path.size()-1);
         
     }
 }
