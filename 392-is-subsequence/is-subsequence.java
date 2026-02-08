29bf1cc2300070688e27fb1a40d637d643b42c2c
Amit Kumar <69569478+amit4996@users.noreply.github.com>
2026-02-08 13:50:21 +0530

    Time: 2 ms (68.22%) | Memory: 43.1 MB (11.17%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,18 +1,35 @@
 class Solution {
+
+    //recursive way
+    // public boolean isSubsequence(String s, String t) {
+    //     int idxS=0;
+    //     int idxT=0;
+    //     return helper(s,t,idxS,idxT);
+    // }
+
+    // public boolean helper(String s,String t,int idxS,int idxT){
+    //     if(idxS==s.length()) return true;
+    //     if(idxT==t.length()) return false;
+    //     if(s.charAt(idxS)==t.charAt(idxT)) {
+    //         return helper(s,t,++idxS,++idxT);
+    //     }else{
+    //         return helper(s,t,idxS,++idxT);
+    //     }
+    // }
+
+
+    //iterative
     public boolean isSubsequence(String s, String t) {
         int idxS=0;
         int idxT=0;
-        return helper(s,t,idxS,idxT);
-    }
-
-    public boolean helper(String s,String t,int idxS,int idxT){
-        if(idxS==s.length()) return true;
-        if(idxT==t.length()) return false;
-        if(s.charAt(idxS)==t.charAt(idxT)) {
-            return helper(s,t,++idxS,++idxT);
-        }else{
-            return helper(s,t,idxS,++idxT);
+    
+        while(idxS<s.length() && idxT<t.length()){
+        if(s.charAt(idxS)==t.charAt(idxT)){
+               idxS++;
         }
+        idxT++;
+        }
+        return idxS==s.length();
     }
     
 }
