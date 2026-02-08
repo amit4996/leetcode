3bef141da964ae8fd89de337146da2a5d6269265
Amit Kumar <69569478+amit4996@users.noreply.github.com>
2026-02-08 14:12:13 +0530

    Time: 4 ms (51.44%) | Memory: 45 MB (47.21%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,12 +1,30 @@
 class Solution {
+
+    //Doing string concatenation inside loop is costly as in java strings are immutable so 
+    //new string gets created everytime
+    // use functions like Character.isLetterOrDigit() && Character.toLowerCase()
+    // public boolean isPalindrome(String s) {
+    //     String cleanedString="";
+    //     for(int i=0;i<s.length();i++){
+    //         char c=s.charAt(i);
+    //         if(c>='A' && c<='Z') cleanedString+=Character.toLowerCase(c);
+    //         if(c>='a' && c<='z') cleanedString+=c;
+    //         if(c>='0' && c<='9') cleanedString+=c;
+    //     }
+    //     return cleanedString.equals(new StringBuilder(cleanedString).reverse().toString());
+    // }
+
     public boolean isPalindrome(String s) {
-        String cleanedString="";
+        StringBuilder cleanedString= new StringBuilder();
+
         for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(c>='A' && c<='Z') cleanedString+=Character.toLowerCase(c);
-            if(c>='a' && c<='z') cleanedString+=c;
-            if(c>='0' && c<='9') cleanedString+=c;
+           char c = s.charAt(i);
+           if(Character.isLetterOrDigit(c)){
+             cleanedString.append(Character.toLowerCase(c));
+           }
         }
-        return cleanedString.equals(new StringBuilder(cleanedString).reverse().toString());
+
+        return cleanedString.toString().equals(new StringBuilder(cleanedString).reverse().toString());
+
     }
 }
