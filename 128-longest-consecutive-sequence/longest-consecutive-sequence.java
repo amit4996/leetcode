d4efc8cf959b09329b8174516dc27d6ce3f2b505
Amit Kumar <69569478+amit4996@users.noreply.github.com>
2026-02-16 18:08:29 +0530

    Time: 32 ms (41.48%) | Memory: 95.6 MB (58.66%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,28 +1,47 @@
 class Solution {
+    // public int longestConsecutive(int[] nums) {
+    //     if(nums.length==0) return 0;
+    //     PriorityQueue<Integer> pq=new PriorityQueue<>();
+
+    //     for(int i=0;i<nums.length;i++){
+    //         pq.add(nums[i]);
+    //     }
+
+    //     int count=0;
+    //     int currCount=0;
+    //     Integer prev=null;
+
+    //     while(!pq.isEmpty()){
+    //          int n1=pq.poll();
+    //          if(prev!=null){
+    //             if(n1-prev==1)
+    //                 currCount++;
+    //             else if(n1==prev) continue;
+    //             else currCount=0;
+    //          }
+    //          count=Math.max(count,currCount);
+    //          prev=n1;
+    //     }
+
+    //     return count+1;
+    // }
+
     public int longestConsecutive(int[] nums) {
-        if(nums.length==0) return 0;
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        Set<Integer> s = new HashSet<>();
 
-        for(int i=0;i<nums.length;i++){
-            pq.add(nums[i]);
-        }
-
+        for(int i=0;i<nums.length;i++) s.add(nums[i]);
         int count=0;
-        int currCount=0;
-        Integer prev=null;
-
-        while(!pq.isEmpty()){
-             int n1=pq.poll();
-             if(prev!=null){
-                if(n1-prev==1)
-                    currCount++;
-                else if(n1==prev) continue;
-                else currCount=0;
-             }
-             count=Math.max(count,currCount);
-             prev=n1;
+        for(int num:s){
+            if(!s.contains(num-1)){
+               int currCount=1;
+               int curr=num;
+               while(s.contains(curr+1)) {
+                currCount++;
+                curr++;
+               }
+               count=Math.max(count,currCount);
+            }
         }
-
-        return count+1;
+        return count;
     }
 }
